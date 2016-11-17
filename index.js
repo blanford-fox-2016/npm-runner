49f96aa3d5b138b1671da717ab19daec99afbf90
tevinstein <tevinsa@yahoo.com>
2016-11-17 11:27:28 +0700

    package

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var compressor = require('node-minify')
+
 new compressor.minify({
   type: 'yui-js',
   fileIn: ['./src/js/reverse.js', './src/js/reverse.js'],
-  fileOut: './src/js/script.min.js',
+  fileOut: './src/minified/script.min.js',
   callback: function(err, min){
     console.log(err);
+    //console.log(min);
   }
 });
+
 new compressor.minify({
   type: 'yui-css',
   fileIn: ['./src/css/site.css', './src/css/site.css'],
-  fileOut: './src/css/style.min.css',
+  fileOut: './src/minified/style.min.css',
     callback: function(err, min){
+      console.log('Clean-css');
       console.log(err);
+      //console.log(min);
     }
 });
