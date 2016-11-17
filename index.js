1a62e17bbdf5f3be57ff9a921cc1f565f61149cb
tevinstein <tevinsa@yahoo.com>
2016-11-17 11:31:40 +0700

    revision

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var compressor = require('node-minify')
 new compressor.minify({
   type: 'yui-js',
   fileIn: ['./src/js/reverse.js', './src/js/reverse.js'],
-  fileOut: './src/minified/script.min.js',
+  fileOut: './src/js/script.min.js',
   callback: function(err, min){
     console.log(err);
     //console.log(min);
@@ -13,7 +13,7 @@ new compressor.minify({
 new compressor.minify({
   type: 'yui-css',
   fileIn: ['./src/css/site.css', './src/css/site.css'],
-  fileOut: './src/minified/style.min.css',
+  fileOut: './src/css/style.min.css',
     callback: function(err, min){
       console.log('Clean-css');
       console.log(err);
